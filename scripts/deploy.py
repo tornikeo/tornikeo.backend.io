baeb0c96008279c9af648ac8efa9e8f065be4b27
tornikeo <tornikeonoprishvili@gmail.com>
2022-11-13 21:54:59 +0400

    Fix deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,11 +1,12 @@
 import os
 import subprocess
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 
 if __name__ == "__main__":
-    load_dotenv(".env")
-    subprocess.run(
-        f"""gcloud run deploy backend --source=$(pwd) --allow-unauthenticated \
-        --update-secrets=
-        --region=us-central1 \
-        --quiet""")
+    env = dotenv_values(".env")
+    env_vars_arg = ','.join([f'{k}="{v}"' for k,v in env.items()])
+    os.system('docker compose build')
+    os.system(f"""gcloud run deploy backend --source=$(pwd) --allow-unauthenticated --region=us-central1 --quiet""")
+        # --update-env-vars={env_vars_arg} \
+        # --region=us-central1 \
+        # --zone=us-central1-a \
